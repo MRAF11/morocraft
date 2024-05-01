faa9ab1a49c5bbc6ffd7ef965808bd23f26a3db4
Mohamed <rahiff112233@gmail.com>
2024-05-01 03:19:48 +0100

    modif

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 gsap.to("#text", {
-  duration: 1, // Durée de l'animation (en secondes)
+  duration: 10, // Durée de l'animation (en secondes)
   x: "15.5%", // Déplacement horizontal vers le centre de l'écran
   ease: "power2.out" // Fonction d'accélération de l'animation
 });
