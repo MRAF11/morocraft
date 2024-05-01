be95c7e200d0141ababb2a7a67e4ccbb3dd3b528
Mohamed <rahiff112233@gmail.com>
2024-05-01 02:20:18 +0100

    enleve php

--- script.js
+++ script.js
@@ -4,9 +4,3 @@ gsap.to("#text", {
   ease: "power2.out" // Fonction d'accélération de l'animation
 });
 
-gsap.to("#logo", {
-  scale: 4, // Échelle finale (sans zoom)
-  opacity: 1, // Opacité finale
-  duration: 1, // Durée de l'animation (en secondes)
-  ease: "power2.out" // Fonction d'accélération de l'animation
-});
